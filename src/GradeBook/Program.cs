27e83d251ae0300c7367aff9429607df4c158c37
Daniel Antonio de França Cruz <daniel.cruz@wscorp.com.br>
2020-06-16 15:09:13 -0300

    Working with List

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,7 +7,23 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };    
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+            var result = 0.0;
+            foreach(var number in grades) {
+                result += number;
+            }
+
+            Console.WriteLine($"Result = {result}");
+
+            if(args.Length > 0) {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else {
+                Console.WriteLine($"Hello!");
+            }
         }
     }
-}
+    }
