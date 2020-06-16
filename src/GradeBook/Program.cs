d94f03845d4b63b6ec79fd3efc3ceeb758e1b97f
Daniel Antonio de França Cruz <daniel.cruz@wscorp.com.br>
2020-06-16 15:35:24 -0300

    definy methods

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,6 +7,9 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var book = new Book();
+            book.addGrade(89.1);
+
             var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
 
@@ -17,14 +20,8 @@ namespace GradeBook
 
             result = result / grades.Count;
 
-            Console.WriteLine($"The average grade: {result:N3}");
+            Console.WriteLine($"The average grade is: {result:N3}");
 
-            if(args.Length > 0) {
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else {
-                Console.WriteLine($"Hello!");
-            }
         }
     }
     }
