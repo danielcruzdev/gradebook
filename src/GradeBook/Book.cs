79c2d43852c1f8446466a26c3caddc2faa87502e
Daniel Antonio de França Cruz <daniel.cruz@wscorp.com.br>
2020-06-16 15:50:44 -0300

    Construtors

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,10 +1,18 @@
+using System.Collections.Generic;
+
 namespace GradeBook
 {
     class Book
     {
+        public Book() 
+        {
+            grades  = new List<double>();
+        }
         public void addGrade(double grade)
         {
-            
+            grades.Add(grade);
         }
+
+        List<double> grades;
     }
 }
