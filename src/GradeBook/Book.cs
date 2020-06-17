53d5dcbece27f17416662d8c55e159e8fc0092e7
Daniel Cruz <d.afc98@gmail.com>
2020-06-16 21:02:09 -0300

    Final module 4

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,14 +1,33 @@
 using System.Collections.Generic;
-
+using System;
 namespace GradeBook
 {
     class Book
     {
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach(var number in grades) {
+
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+
+            result = result / grades.Count;
+
+            Console.WriteLine($"The Highest Grade is: {highGrade:N1}");
+            Console.WriteLine($"The Lowest Grade is: {lowGrade:N1}");
+            Console.WriteLine($"The Average grade is: {result:N1}");
+        }
         public Book(string name) 
         {
             grades  = new List<double>();
+            this.name = name;
         }
-        public void addGrade(double grade)
+        public void AddGrade(double grade)
         {
             grades.Add(grade);
         }
